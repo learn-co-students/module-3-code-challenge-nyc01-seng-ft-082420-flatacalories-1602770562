90bd6fe7acb777de1263286c0afc18b51df5c38f
gerardmi24 <gerardmistretta@yahoo.com>
2020-10-15 10:26:33 -0400

    First update

---
+++ src/index.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const baseUrl = 'http://localhost:3000/characters'
+    const charDiv = document.getElementById('character-bar')
+
+    const getCharacters = () => {
+        fetch(baseUrl)
+        .then(resp => resp.json())
+        //.then(characters => console.log(characters));
+        .then(characters => renderChars(characters));
+    }
+
+    const renderChars = (characters) => {
+        for(const character of characters){
+            //console.log(character)
+            addCharToDom(character)
+        }
+    }
+
+    const addCharToDom = (character) => {
+        const charSpan = document.createElement('span')
+        charSpan.dataset.id = character.id
+        charSpan.innerHTML = `
+        
+        ${character.name}
+
+        `
+        charDiv.appendChild(charSpan)
+    }
+
+
+
+
+
+    getCharacters();
+})
