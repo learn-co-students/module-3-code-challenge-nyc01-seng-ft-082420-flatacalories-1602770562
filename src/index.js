32bc29530aee4fae6c53f420f869f55cc8008a33
D1yr <alexander.m.sahli@gmail.com>
2020-10-15 10:41:56 -0400

    first deliverable done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(chars => renderChars(chars))
         
     }
+
+    const renderChars = chars => {
+        chars.forEach(char => renderChar(char))
+        
+    }
+
+    const renderChar = char => {
+        const charBar = document.querySelector('#character-bar')
+        const charSpan = document.createElement('span')
+        charSpan.innerHTML = `<span>${char.name}</span>`
+        charBar.append(charSpan)
+        
+    }
+
     
     getChars()
 })
