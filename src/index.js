39614df6b2a4c11ee3defad99e5c519f0139e516
D1yr <alexander.m.sahli@gmail.com>
2020-10-15 11:17:20 -0400

    got name to add on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const baseURL = "http://localhost:3000/"
     const charsURL = "http://localhost:3000/characters"
+    const charBar = document.querySelector('#character-bar')
     console.log("domloaded")
 
     const getChars = () => {
@@ -16,13 +17,32 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const renderChar = char => {
-        const charBar = document.querySelector('#character-bar')
         const charSpan = document.createElement('span')
         charSpan.innerHTML = `<span>${char.name}</span>`
+        const charName = document.querySelector('#name')
+        
         charBar.append(charSpan)
+        
+
         
     }
 
+    const clickHandler = () => {
+        charBar.addEventListener('click', e => {
+            const charSpan = e.target.innerHTML
+            console.log(charSpan)
+            const charName = document.querySelector('#name')
+            charName.innerHTML = `${charSpan}`
+            console.log(charName)
+            
+            
+        })
+
+    }
+
+
+
     
     getChars()
+    clickHandler()
 })
