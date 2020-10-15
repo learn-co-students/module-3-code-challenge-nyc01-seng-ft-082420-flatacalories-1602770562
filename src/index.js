8accc63ec7e671cb9ec2625e9fdb91a471ab8e5a
iuriseara <iuriseara@gmail.com>
2020-10-15 10:53:00 -0400

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
     const baseUrl = 'http://localhost:3000/characters'
     const characterBar = document.querySelector('#character-bar')
+    const characterInfo = document.querySelector('#detailed-info')
+
+    console.log(characterInfo)
 
     const getCharacters = () => {
         fetch(baseUrl)
@@ -26,11 +29,37 @@
         characterBar.addEventListener('click', function (e){
             if (e.target.matches('span')) {
                 let span = e.target
-              let characterId = span.dataset.id
+                let characterId = span.dataset.id
+                getCharacterSpan(characterId)
             }
         })
     }
 
+
+    const getCharacterSpan = (characterId) => {
+        fetch(baseUrl + characterId)
+        .then(response => response.json())
+        .then(character => renderCharacterInfo(character))
+    }
+
+
+    const renderCharacterInfo = (character) => {
+        characterInfo.innerHTML = `
+         <p id="name">${character.name}</p>
+        <img id="image" src="${character.image}">
+        <h4>Total Calories: <span id="calories">${character.calories}</span> </h4>
+                
+        
+        <form id="calories-form">
+            <input type="hidden" value="${character.id}" id="characterId"/> 
+            <input type="hidden" value="${character.name}" id="characterId"/> 
+            <input type="text" placeholder="0" id="calories"/>
+            <input type="submit" value="Add Calories"/>
+        </form>
+
+        <button id="reset-btn">Reset Calories</button>
+        `
+    }
 
 
 
