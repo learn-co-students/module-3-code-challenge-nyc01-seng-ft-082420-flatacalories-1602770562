bac7a94d0fc8a443b32d4bfdc456361bf5c11af8
Sean LaFlam <sean.laflam@gmail.com>
2020-10-15 08:11:56 -0700

    Base Deliverables Complete

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', e => {
   const baseUrl = 'http://localhost:3000/characters/'
 
   const getCharacters = () => {
+    cBar = document.getElementById('character-bar')
+    cBar.innerHTML = ""
     fetch(baseUrl)
     .then(response => response.json())
     .then(characterData => {
-      console.log(characterData)
-      cBar = document.getElementById('character-bar')
-      console.log(cBar)
       for (character of characterData) {
         const cSpan = document.createElement('span')
         cSpan.textContent = character.name
@@ -21,7 +20,7 @@ document.addEventListener('DOMContentLoaded', e => {
     document.addEventListener('click', e => {
       if (e.target.matches('[data-character-id]')) {
         const cButton = e.target
-        const cId = cButton.dataset.characterId
+        cId = cButton.dataset.characterId
 
         fetch(baseUrl + cId)
         .then(response => response.json())
@@ -29,15 +28,80 @@ document.addEventListener('DOMContentLoaded', e => {
           let info = document.getElementById('detailed-info')
           let cName = document.getElementById('name')
           let cImage = document.getElementById('image')
+          let calories = document.getElementById('calories')
+          let form = document.getElementById('calories-form')
+          form.dataset.characterId = cData.id
           cName.textContent = cData.name
           cImage.src = cData.image
+          calories.textContent = cData.calories
         })
+      } else if (e.target.matches('#reset-btn')) {
+        let calories = document.getElementById('calories')
+        calories.textContent = "0"
+        const cButton = e.target
+        cId = cButton.dataset.characterId
+
       }
     })
   }
 
+  // const setCalories = (calories) ={
+  //   options = {
+  //     method: "PATCH",
+  //     headers: {
+  //       "content-type" : "application/json",
+  //       "accept" : "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       calories: calories
+  //     })
+  //   }
+  //
+  //
+  //   fetch(baseUrl + cId, options)
+  //   .then(response => response.json())
+  //   .then(cData => {
+  //     let calories = document.getElementById('calories')
+  //     calories.textContent = cData.calories
+  //   })
+  // }
+
+  const submitHandler = () => {
+    const caloriesForm = document.getElementById('calories-form')
+    caloriesForm.addEventListener('submit', e => {
+      e.preventDefault()
+
+      const calorieCount = caloriesForm.querySelector('#calories').value
+      const cId = caloriesForm.dataset.characterId
+      // console.log(calorieCount)
+      // console.log(cId)
+
+      // setCalories(calorieCount)
+
+      options = {
+        method: "PATCH",
+        headers: {
+          "content-type" : "application/json",
+          "accept" : "application/json"
+        },
+        body: JSON.stringify({
+          calories: calorieCount
+        })
+      }
+
+
+      fetch(baseUrl + cId, options)
+      .then(response => response.json())
+      .then(cData => {
+        let calories = document.getElementById('calories')
+        calories.textContent = cData.calories
+      })
+    })
+
+  }
 
   getCharacters()
   clickHandler()
+  submitHandler()
 
 })
