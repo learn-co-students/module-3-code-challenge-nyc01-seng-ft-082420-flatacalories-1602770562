f0a655718bac43c6939e753433b029c7df73b2a7
gabrielhicks <gabrielhicks@mail.com>
2020-10-15 10:29:01 -0400

    working on calorie form

---
+++ src/index.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getCharacters()
+    clickHandler()
+    submitHandler()
+})
+CHAR_URL = "http://localhost:3000/characters/"
+CHARACTERS = []
+
+function getCharacters() {
+    fetch(CHAR_URL)
+    .then(response => response.json())
+    .then(chars => {
+        CHARACTERS = chars
+        renderCharacters(CHARACTERS)
+    })
+}
+
+function renderCharacters(characters) {
+    for(character of characters) {
+        const charDiv = document.querySelector("#character-bar")
+        renderCharacterSpan(character, charDiv)
+    }
+}
+
+function renderCharacterSpan(character, div) {
+    const charSpan = document.createElement("span")
+    charSpan.innerText = character.name
+    charSpan.dataset.id = character.id
+    div.append(charSpan)
+}
+
+function renderCharacterInfo(character, div) {
+    // console.log(character)
+    // console.log(div)
+    for(char of CHARACTERS) {
+        if(character.innerText == char.name) {
+            const name = div.children[0]
+            const header = div.children[2]
+            div.children[0].innerText = char.name
+            div.children[1].src = char.image
+            header.children[0].innerText = char.calories
+        }
+    }
+}
+
+function clickHandler() {
+    document.addEventListener('click', e => {
+        if(e.target.matches("span")) {
+            const char = e.target
+            const infoDiv = document.querySelector("#detailed-info")
+            // console.log(e.target)
+            renderCharacterInfo(char, infoDiv)
+        }
+    })
+}
+
+function submitHandler() {
+    document.addEventListener('submit', e => {
+        e.preventDefault()
+        if(e.target.matches("form")) {
+            console.log(e.target)
+        }
+    })
+}
