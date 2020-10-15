097e02cc4c2503294a688d05ac365c34de72499b
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-10-15 11:13:07 -0400

    Core deliverable & Reset Btn

--- src/index.js
+++ src/index.js
@@ -36,25 +36,64 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(resp => resp.json())
     .then(renderDogDetail)
   }
+  const patchDog = (id, adds) => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'accepts': 'application/json'
+      },
+      body: JSON.stringify({calories: parseInt(adds)})
+    }
 
+    fetch(PRE_FIX + id, options)
+    .then(resp => resp.json())
+    .then(renderCalories)
+  }
+
+  const renderCalories = (dog) => {
+    const caloriesSpan = document.querySelector('span#calories')
+    caloriesSpan.innerText = dog.calories
+  }
+  
   const renderDogDetail = (dog) => {
     const name = detailDiv.querySelector('#name')
     const image = detailDiv.querySelector('#image')
     const calories = detailDiv.querySelector('#calories')
+    const hiddenId = detailDiv.querySelector('#characterID')
     name.innerHTML = dog.name
     image.setAttribute('src', dog.image)
     calories.innerHTML = dog.calories
+    hiddenId.value = dog.id
   }
   const clickHandler = () => {
-    document.addEventListener('click', (e) => {
+    document.addEventListener('click', e => {
       let data = e.target
       if(data.matches('#character-bar span')){
         let dogId = data.dataset.id
         fetchDog(dogId)
+      }else if(data.matches('#reset-btn')){
+        let dogId = document.querySelector('#characterId').value
+        patchDog(dogId, 0)
+      }
+
+    })
+  }
+  const formHandler = () => {
+    document.addEventListener('submit', e => {
+      let form = e.target
+      if(form.matches('#calories-form')){
+        const id = form.querySelector('#characterId').value
+        const add = form.querySelector('#calories').value
+        const origin = document.querySelector('span#calories').innerText
+        const sum = parseInt(add) + parseInt(origin)
+        patchDog(id, sum);
+        e.preventDefault();
       }
     })
   }
 
+  formHandler();
   fetchDogs();
   clickHandler();
 
