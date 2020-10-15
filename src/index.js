9e09bdf0deedd23a1b323b93bfd681ba5fc3ce79
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-15 10:58:09 -0400

    finished core deliverables, working on advanced

---
+++ src/index.js
@@ -0,0 +1,98 @@
+/*
+## Core Deliverables
+
+As a user, I can:
+
+3. Clicks on "Add Calories" button to add calories to a Character. Persist calories value to the server and update the DOM.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const BASEURL = "http://localhost:3000/characters/"
+
+    const getCharacters = () => {
+        fetch(BASEURL)
+        .then(response => response.json())
+        .then(characterData => renderCharacters(characterData))
+    }
+
+    const renderCharacters = (characterData) => {
+        const characterBar = document.querySelector("#character-bar")
+        for(const character of characterData) {
+            renderCharacter(character, characterBar)
+        }
+    }
+
+    const renderCharacter = (character, characterBar) => {        
+        const characterSpan = document.createElement("span")
+        characterSpan.dataset.id = character.id
+        characterSpan.classList.add("charSpan")
+        characterSpan.innerHTML = `
+        ${character.name}
+        `
+
+        characterBar.appendChild(characterSpan)
+    }
+
+    const clickHandler = () => {
+        document.addEventListener("click", e => {
+            if(e.target.matches(".charSpan")) {
+                const spanButton = e.target
+                const characterId = spanButton.dataset.id
+
+                fetch(BASEURL + characterId)
+                .then(response => response.json())
+                .then(characterObj => renderCharacterDiv(characterObj))
+            }
+        })
+    }
+
+    const renderCharacterDiv = (characterObj) => {
+        const detailedInfoDiv = document.querySelector("#detailed-info")
+        detailedInfoDiv.dataset.id = characterObj.id 
+
+        detailedInfoDiv.innerHTML = `
+            <p id="name">${characterObj.name}</p>
+            <img id="image" src=${characterObj.image}><!-- display character image here -->
+            <h4>Total Calories: <span id="calories">${characterObj.calories}</span> </h4>
+            <form id="calories-form">
+                <input type="hidden" value="Character's id" id="${characterObj.id}"/> <!-- Assign character id as a value here -->
+                <input type="text" placeholder="Enter Calories" id="calories"/>
+                <input type="submit" value="Add Calories"/>
+            </form>
+            <button id="reset-btn">Reset Calories</button>
+        `
+
+    }
+
+    const submitHandler = () => {
+        document.addEventListener("submit", e => {
+            e.preventDefault()
+            const form = document.querySelector("#calories-form")
+            const currentCalories = document.querySelector("#calories").innerText
+            const currentCaloriesInt = parseInt(currentCalories)
+            const caloriesInput = form.querySelector("#calories")
+            const caloriesInputInt = parseInt(caloriesInput.value)
+            const newCalories = caloriesInputInt + currentCaloriesInt
+
+            const submitButton = e.target
+            const characterId = submitButton.closest("div").dataset.id
+
+            options = {
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify({ calories: newCalories})
+            }
+
+            fetch(BASEURL + characterId, options)
+            .then(response => response.json())
+            .then(characterObj => renderCharacterDiv(characterObj))
+        })
+    }
+
+    getCharacters();
+    clickHandler();
+    submitHandler();
+})
