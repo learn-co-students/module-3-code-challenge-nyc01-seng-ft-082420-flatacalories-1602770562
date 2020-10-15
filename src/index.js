3e8c7daebd6934f38de7ad148defba9b7003a530
AJavier55 <aliciajavier5@gmail.com>
2020-10-15 11:42:00 -0400

    ran out of time...again

--- src/index.js
+++ src/index.js
@@ -18,10 +18,11 @@ document.addEventListener("DOMContentLoaded", e => {
         const nameSpan = document.createElement("span")
         nameSpan.dataset.characterId = character.id
         nameSpan.classList.add("character-name")
-
         nameSpan.textContent = `${character.name}`
 
-        nameDiv.append(nameSpan)   
+        nameDiv.append(nameSpan)  
+        
+        
     };
 
     const clickHandler = () => {
@@ -29,13 +30,53 @@ document.addEventListener("DOMContentLoaded", e => {
             if (e.target.matches(".character-name")){
                 const span = e.target
                 const characterId = span.dataset.characterId
-                const infoDIv = document.querySelector("#detailed-info")
-                console.log(infoDIv)
-                console.log(characterId)
+                fetch(baseUrl + characterId)
+                .then(res => res.json())
+                .then(character => CharacterInfo(character))
             }
         })
     };
 
+    const CharacterInfo = character => {
+        const infoDiv = document.querySelector("#detailed-info")
+        const cells = infoDiv.children
+        const spanCalories = document.querySelector("#calories")
+        
+        const name = character.name
+        cells[0].textContent = name
+        const img = character.image
+        cells[1].src = img
+        const calories = character.calories
+        spanCalories.textContent = calories
+    }
+
+    const submitHandler = () => {
+        document.addEventListener("submit", e =>{
+            e.preventDefault();
+            const form = e.target
+           const calories = form[1].value
+           const newCalories = parseInt(calories.value) + 1
+           const character = document.querySelector(".character-name")
+           const characterId = character.dataset.characterId
+            
+           const options  = {
+               method: "PATCH",
+               headers: {
+                   "content-type": "application/json",
+                   "accepts": "application/json"
+               },
+               body: JSON.stringify({calories: newCalories})
+           }
+           fetch(baseUrl + characterId, options)
+           .then(res => res.json())
+           .then(data => {
+               calories.value = `${data.calories}`
+           })
+            
+        })
+    }
+
     getCharacters();
     clickHandler();
+    submitHandler();
 });
