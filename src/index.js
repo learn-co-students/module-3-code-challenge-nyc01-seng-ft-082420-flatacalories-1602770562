631e6ffac6ca6400d0c94b0cbcd613f2136c9fdd
gerardmi24 <gerardmistretta@yahoo.com>
2020-10-15 10:45:06 -0400

    Progress Report

--- src/index.js
+++ src/index.js
@@ -20,16 +20,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const charSpan = document.createElement('span')
         charSpan.dataset.id = character.id
         charSpan.innerHTML = `
-        
         ${character.name}
-
         `
         charDiv.appendChild(charSpan)
     }
 
+    const clickCharacter = () => {
+    const charInfoDiv = document.getElementById('detailed-info')
+    //const imageId = document.getElementById('image')
+        charDiv.addEventListener('click', (e) => {
+    //        document.getElementById("character-bar").click();
+    //        if e.target = character.name
+
+        })
+    }
 
 
-
-
-    getCharacters();
+    getCharacters()
+    clickCharacter()
 })
